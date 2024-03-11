061cdde2c7f5438b936c7adec674e06cbdeb0b13
Raul Sigoli <raul_sigoli@hotmail.com>
2024-03-10 23:48:28 -0300

    feat: Cadastro de restaurante
    Nesta aula, vamos criar o processo de cadastro da aplicação, seguindo o mesmo padrão utilizado para o login. Vamos criar um arquivo chamado "register-restaurant.ts" para lidar com o cadastro de restaurantes. Vamos copiar o código do arquivo de login e substituir todas as ocorrências de "sign-in" por "registerRestaurant". Vamos adicionar algumas informações extras, como o nome do restaurante, nome do gerente, email e telefone. Faremos uma requisição POST para a rota "/restaurant" enviando essas informações. Vamos manter o código mais visual, mesmo que pareça repetitivo. Ao finalizar o cadastro, redirecionaremos o usuário para a página de login com o email preenchido automaticamente.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -17,11 +17,17 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    },
+  })
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
@@ -52,6 +58,7 @@ export function SignIn() {
         <Button variant="ghost" asChild className="absolute right-8 top-8">
           <Link to="/sign-up">Novo estabelecimento</Link>
         </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
